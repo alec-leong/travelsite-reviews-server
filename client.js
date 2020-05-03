16601a471f65ad60668a317363eb871ccf276f47
Alec Leong <leongalec@gmail.com>
2020-05-02 17:30:01 -0700

    Client: GET request by listing ID

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
+const id = "5eae0aa5a1eb902fe8b9674b";
 
-axios.get('http://localhost:3000/')
+axios.get(`http://localhost:3000/${id}`)
   .then(({data}) => console.log(data))
   .catch(err => console.error(err))
