ab372d4a3644ceceafb703b1d1dac9d806ecf4d4
Alec Leong <leongalec@gmail.com>
2020-05-06 07:40:51 -0700

    Added Styling using style-components Module

---
+++ client/src/css/style.jsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+// const Font = styled('font')`
+//   font-family: 'Trip Sans', Arial, sans-serif !important;
+//   font-size: 14px;
+// `;
+
+// reviews
+const Header2 = styled.h2`
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  font-size: 28px;
+  color: #000a12;
+`;
+
+// <p> tag
+const P = styled.p`
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  font-weight: 700;
+  color: rgb(0, 10, 18);
+`;
+
+// checkbox, radio
+const Label = styled.label`
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  color: rgb(0, 0, 0)
+`;
+
+// number 
+const Span = styled.span`
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  color: rgb(118, 118, 118);
+`;
+export {
+  Header2, 
+  Label,
+  P,
+  Span,
+}
+
+/*
+#langHeader {
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  font-weight: 700;
+  color: rgb(0, 10, 18);
+}
+
+.lang {
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  color: rgb(0, 0, 0)
+}
+
+.langCount {
+  font-family: 'Trip Sans', Arial, sans-serif !important;
+  font-size: 14px;
+  color: rgb(118, 118, 118);
+}
+*/
