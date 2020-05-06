d718a1b3c63641afe93c73d9d73d292a1227e26e
Alec Leong <leongalec@gmail.com>
2020-05-05 22:00:42 -0700

    Fix GET request

--- client/clientGetReq.js
+++ client/clientGetReq.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const id = process.argv[2]; // example: 5eae0aa5a1eb902fe8b9674b
+const id = 7;
 
 axios.get(`http://localhost:3000/${id}`)
   .then(({ data }) => console.log(data))
