72885c3843b60f20dca3e8157182b0541f49808b
Alec Leong <leongalec@gmail.com>
2020-05-08 17:24:29 -0700

    Made Languages to be Functional instead of Stateful

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,27 +5,48 @@ import { types, times, languages } from '../helpers/reviewsGridConfig';
 import TravelerType from './TravelerType';
 
 // import Header from './Header';
-// import Languages from './Languages';
+import Languages from './Languages';
 import TimeOfYear from './TimeOfYear';
 // import TravelerType from './TravelerType';
 
 class App extends Component {
+  /**
+   * Constructor
+   * @param {Object} props - Short for properties; has all the values passed from parent component.
+   */
   constructor(props) {
-    super(props);
+    super(props); // Sets `this.props`. Otherwise, when accessing `this.props`, would be `undefined`
 
     this.state = {
       types,
       times,
       languages,
+      selectedLang: '',
       reviews: [],
     };
 
+    this.handleLangChange = this.handleLangChange.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
     this.handleTravelerChange = this.handleTravelerChange.bind(this);
   }
 
+
   /**
-   * Handle change in TravelerType's `props`
+   * Handle change in Languages' `props`
+   * @param {Object} target - The `target` property of the `Event` interface is a reference to the
+   *                          object onto which the event was dispatched.
+   * @param {String} name - The native DOM `name` attribute.
+   * @param {String} selectedLang - An alias for `name` parameter.
+   */
+  handleLangChange({ target: { name: selectedLang } }) {
+    this.setState({
+      selectedLang,
+    });
+  }
+
+
+  /**
+   * Handle change in TimeOfYear's `props`
    * @param {Object} event - The `Event` interface; a reference to the object onto which the event
    *                         was dispatched.
    */
@@ -42,6 +63,7 @@ class App extends Component {
       times,
     });
   }
+
 
   /**
    * Handle change in TravelerType's `props`
@@ -63,6 +85,7 @@ class App extends Component {
     });
   }
 
+
   // initial
   componentDidMount() {
     axios.get('/reviews')
@@ -70,12 +93,14 @@ class App extends Component {
       .catch(console.error);
   }
 
+
   render() {
-    const { reviews, times, types } = this.state;
+    const { languages, reviews, selectedLang, times, types } = this.state;
     return (
       <div>
         <TravelerType types={types} handleChange={this.handleTravelerChange} />
         <TimeOfYear times={times} handleChange={this.handleTimeChange} />
+        <Languages languages={languages} selected={selectedLang} handleChange={this.handleLangChange} />
         <ReviewList reviews={reviews} />
       </div>
     );
--- client/src/components/Languages.jsx
+++ client/src/components/Languages.jsx
@@ -1,84 +1,27 @@
 import React from 'react';
 import { Label, P, Span } from '../css/style';
 
-const data = [
-  { 'All languages': 1586 },
-  { English: 1557 },
-  { Spanish: 11 },
-  { French: 3 },
-  { Hebrew: 3 },
-  { Portuguese: 3 },
-  { Danish: 1 },
-  { German: 1 },
-  { Italian: 1 },
-  { Japanese: 1 },
-  { Korean: 1 },
-  { Dutch: 1 },
-  { Polish: 1 },
-  { Swedish: 1 },
-  { 'Chinese (Sim.)': 1 },
-];
+const Languages = ({ languages, selected, handleChange }) => (
+  <form>
+    <P>Language</P>
+    {languages.map((lang, i) => {
+      const [key] = Object.keys(lang);
 
-class Languages extends React.Component {
-  /**
-   * Constructor
-   * @param {Object} props - Short for properties; has all the values passed from parent component.
-   */
-  constructor(props) {
-    super(props); // Sets `this.props`. Otherwise, when accessing `this.props`, would be `undefined`
-
-    this.state = {
-      selected: '',
-    };
-
-    this.handleInputRadioChange = this.handleInputRadioChange.bind(this);
-  }
-
-  /**
-   * Handling Events: handle the change in input radio selection of languages.
-   * @param {Object} target - The `target` property of the `Event` interface is a reference to the
-   *                          object onto which the event was dispatched.
-   * @param {String} name - The `name` attribute.
-   * @param {String} selected - An alias for `name` parameter.
-   */
-  handleInputRadioChange({ target: { name: selected } }) {
-    this.setState({
-      selected,
-    });
-  }
-
-  /**
-   * Render component
-   * @returns {JSX} A virtual DOM element
-   */
-  render() {
-    const { selected } = this.state;
-
-    return (
-      <div>
-        <P>Language</P>
-        <form>
-          {data.map((lang, i) => {
-            const [key] = Object.keys(lang);
-
-            return (
-              <Label key={i}>
-                <input
-                  type="radio"
-                  name={key}
-                  checked={selected === key}
-                  onChange={this.handleInputRadioChange}
-                />
-                {key}
-                <Span> ({lang[key]})</Span>
-                <br />
-              </Label>
-            );
-          })}
-        </form>
-      </div>
-    );
-  }
-}
+      return (
+        <Label key={i}>
+          <input
+            type="radio"
+            name={key}
+            checked={selected === key}
+            onChange={handleChange}
+          />
+          {key}
+          <Span> ({lang[key]})</Span>
+          <br />
+        </Label>
+      );
+    })}
+  </form>
+);
 
 export default Languages;
