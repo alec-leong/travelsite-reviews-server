35573d2cbf2348ec88dfa71b2fb64d43cafe1e63
Alec Leong <leongalec@gmail.com>
2020-05-17 22:47:52 -0700

    removed external style sheet

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,12 +1,13 @@
 import React  from 'react';
-import { FormSearch, InputSearch } from '../css/style';
+import { FormSearch, InputSearch, LabelSearch } from '../css/style';
 
 const Search = ({ handleChange, handleSubmit }) => (
   <FormSearch onSubmit={handleSubmit}>
-    &#x1f50e;
-    <InputSearch
-      onChange={handleChange}
-    />
+    <LabelSearch>'
+      <InputSearch
+        onChange={handleChange}
+      />
+    </LabelSearch>
   </FormSearch>
 );
 
