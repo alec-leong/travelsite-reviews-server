b43ef463de97c126af1e2f5d5a8edcd6c88d6f4a
Alec Leong <leongalec@gmail.com>
2020-06-13 02:17:46 -0700

    SPDY server to HTTP

--- server/config.js
+++ server/config.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 const OPTIONS = {
-  key: fs.readFileSync(path.join(__dirname, '../localhost-privkey.pem')), 
-  cert: fs.readFileSync(path.join(__dirname, '../localhost-cert.pem')),
   spdy: {
     plain: true,
     ssl: false,
