e10edd76e9be78302973d1cff81e55d97d7d0375
Alec Leong <leongalec@gmail.com>
2020-06-12 23:54:13 -0700

    spdy server configuration to http

--- server/config.js
+++ server/config.js
@@ -4,6 +4,10 @@ const path = require('path');
 const OPTIONS = {
   key: fs.readFileSync(path.join(__dirname, '../localhost-privkey.pem')), 
   cert: fs.readFileSync(path.join(__dirname, '../localhost-cert.pem')),
+  spdy: {
+    plain: true,
+    ssl: false,
+  },
 }; 
 
 const PORT = 3004;
