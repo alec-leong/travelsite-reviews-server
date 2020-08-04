c7f9848a81472fb9a8087caeaaee0633c90d2ce7
Alec Leong <leongalec@gmail.com>
2020-08-03 18:20:15 -0700

    ESLint configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -22,6 +23,5 @@ module.exports = {
     'react',
   ],
   rules: {
-    "no-console": "off",
   },
 };
