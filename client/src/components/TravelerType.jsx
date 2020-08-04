78c1225d8558bb05decaeb3c1944d165eb6aa693
Alec Leong <leongalec@gmail.com>
2020-08-03 18:04:08 -0700

    <TravelerType /> renders checkbox <input> elements.

--- client/src/components/TravelerType.jsx
+++ client/src/components/TravelerType.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import sha256 from 'crypto-js/sha256';
 import { Label, P } from '../css/style';
 
-const TravelerType = ({ types, handleChange }) => (
+const TravelerType = ({ travelerType, handleChange }) => (
   <form>
-    <P>Travel type</P>
-    {types.map((month, i) => {
-      const [key] = Object.keys(month);
-
-      return (
-        <Label key={i}>
-          <input
-            type="checkbox"
-            index={i}
-            name={key}
-            checked={month[key]}
-            onChange={handleChange}
-          />
-          {key}
-          <br />
-        </Label>
-      );
-    })}
+    <P>Traveler type</P>
+    {Object.entries(travelerType).map(([type, isSelected]) => (
+      <Label key={sha256(type, 'traveler type').toString()}>
+        <input
+          type="checkbox"
+          name={type}
+          checked={isSelected}
+          onChange={handleChange}
+        />
+        {type}
+        <br />
+      </Label>
+    ))}
   </form>
 );
+
+TravelerType.propTypes = {
+  travelerType: PropTypes.objectOf(PropTypes.bool).isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default TravelerType;
