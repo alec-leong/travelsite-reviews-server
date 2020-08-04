efb91628912fc60dee9bc109b76f4afc0e1e4789
Alec Leong <leongalec@gmail.com>
2020-08-03 18:17:06 -0700

    SPDY server configuration.

--- server/config.js
+++ server/config.js
@@ -2,16 +2,29 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 
-const OPTIONS = {
+let key = '';
+let cert = '';
+let plain = true;
+let ssl = false;
+
+if (/^(https|HTTP\/2|HTTP2)/i.test(process.env.APP_PROTOCOL)) {
+  try {
+    key = fs.readFileSync(__dirname, '../keys/localhost-privkey.pem');
+    cert = fs.readFileSync(path.join(__dirname, '../keys/localhost-cert.pem'));
+    plain = !plain;
+    ssl = !ssl;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const spdyOptions = {
+  key,
+  cert,
   spdy: {
-    plain: true,
-    ssl: false,
+    plain,
+    ssl,
   },
-}; 
-
-const PORT = process.env.SERVER_PORT || 3000;
-
-module.exports = {
-  OPTIONS,
-  PORT,
 };
+
+module.exports = spdyOptions;
