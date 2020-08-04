da87fb3acaf06d8a9e98313185e76d2c20cfbb31
Alec Leong <leongalec@gmail.com>
2020-08-03 17:58:23 -0700

    <Languages /> renders radio <input> elements.

--- client/src/components/Languages.jsx
+++ client/src/components/Languages.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import sha256 from 'crypto-js/sha256';
 import { Label, P, Span } from '../css/style';
 
-const Languages = ({ languages, selected, handleChange }) => (
+const Languages = ({ language, selected, handleChange }) => (
   <form>
     <P>Language</P>
-    {languages.map((lang, i) => {
-      const [key] = Object.keys(lang);
-
-      return (
-        <Label key={i}>
-          <input
-            type="radio"
-            name={key}
-            checked={selected === key}
-            onChange={handleChange}
-          />
-          {key}
-          <Span> ({lang[key]})</Span>
-          <br />
-        </Label>
-      );
-    })}
+    {Object.entries(language).map(([lang, count]) => (
+      <Label key={sha256(lang, 'language').toString()}>
+        <input
+          type="radio"
+          name={lang}
+          checked={selected === lang}
+          onChange={handleChange}
+        />
+        {lang}
+        &nbsp;
+        <Span>{count}</Span>
+        <br />
+      </Label>
+    ))}
   </form>
 );
+
+Languages.propTypes = {
+  language: PropTypes.objectOf(PropTypes.number).isRequired,
+  selected: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default Languages;
