f4a9a851fdd34d53f9ada08cac60e394c4157405
Alec Leong <leongalec@gmail.com>
2020-08-03 18:03:08 -0700

    <TimeOfYear /> renders checkbox <input> elements.

--- client/src/components/TimeOfYear.jsx
+++ client/src/components/TimeOfYear.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import sha256 from 'crypto-js/sha256';
 import { Label, P } from '../css/style';
 
-const TimeOfYear = ({ times, handleChange }) => (
+const TimeOfYear = ({ timeOfYear, handleChange }) => (
   <form>
     <P>Time of year</P>
-    {times.map((month, i) => {
-      const [key] = Object.keys(month);
-
-      return (
-        <Label key={i}>
-          <input
-            type="checkbox"
-            index={i}
-            name={key}
-            checked={month[key]}
-            onChange={handleChange}
-          />
-          {key}
-          <br />
-        </Label>
-      );
-    })}
+    {Object.entries(timeOfYear).map(([rangeOfMonths, isSelected]) => (
+      <Label key={sha256(rangeOfMonths, 'time of year').toString()}>
+        <input
+          type="checkbox"
+          name={rangeOfMonths}
+          checked={isSelected}
+          onChange={handleChange}
+        />
+        {rangeOfMonths}
+        <br />
+      </Label>
+    ))}
   </form>
 );
+
+TimeOfYear.propTypes = {
+  timeOfYear: PropTypes.objectOf(PropTypes.bool).isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 
 export default TimeOfYear;
