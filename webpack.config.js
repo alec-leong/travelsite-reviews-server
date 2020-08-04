ae804199901ddd69550cf41a3cbb33cb32fdfe9c
Alec Leong <leongalec@gmail.com>
2020-08-03 18:22:43 -0700

    Webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const dotenv = require('dotenv');
 const path = require('path');
+const webpack = require('webpack');
 
 const SRC_DIR = path.join(__dirname, 'client/src');
 const DIST_DIR = path.join(__dirname, 'client/dist');
+
+const env = dotenv.config().parsed;
+const envVariableNamesAndValues = Object.keys(env).reduce((accumulator, envVariableName) => {
+  accumulator[`process.env.${envVariableName}`] = JSON.stringify(env[envVariableName]);
+  return accumulator;
+}, {});
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -36,5 +44,11 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.DefinePlugin(envVariableNamesAndValues),
+  ],
+  node: {
+    fs: 'empty',
   },
 };
